ef294fe3cd9949cbdc6a369e46cb51275c08a845
geswanel <geswanel@gmail.com>
2023-12-11 16:51:59 +0700

    Creating post models, creating posts by shell, registering model, template filters

--- tutorial_project/blog/views.py
+++ tutorial_project/blog/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-posts = [
-    {"author": "Jane Foster",
-     "date_posted": "September 8, 2023",
-     "title": "Blog post 1",
-     "content": "blog post 1 content"},
-    {"author": "Robert Smith",
-     "date_posted": "August 10, 2022",
-     "title": "Blog post 2",
-     "content": "blog post 2 content"},
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
     return render(request, "blog/home.html", context)
 
