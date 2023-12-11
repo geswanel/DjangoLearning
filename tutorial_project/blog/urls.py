08fa38b770f98936fb4b1e0395b56c47b8fab1d2
geswanel <geswanel@gmail.com>
2023-12-11 14:47:29 +0700

    url replacement + app_name for distinguishing urls

--- tutorial_project/blog/urls.py
+++ tutorial_project/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path("", views.home, name="blog-home"),  # naming?
-    path("about/", views.about, name="blog-about")
+    path("", views.home, name="home"),  # naming?
+    path("about/", views.about, name="about")
 ]
