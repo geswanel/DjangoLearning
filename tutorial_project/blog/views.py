a5a112df49cc7bb55548de597fb672d4666e6cf7
geswanel <geswanel@gmail.com>
2023-12-11 14:30:15 +0700

    dummy data passing, adjusting templates

--- tutorial_project/blog/views.py
+++ tutorial_project/blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {"author": "Jane Foster",
+     "date_posted": "September 8, 2023",
+     "title": "Blog post 1",
+     "content": "blog post 1 content"},
+    {"author": "Robert Smith",
+     "date_posted": "August 10, 2022",
+     "title": "Blog post 2",
+     "content": "blog post 2 content"},
+]
+
 # Create your views here.
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
     return render(request, "blog/about.html")
