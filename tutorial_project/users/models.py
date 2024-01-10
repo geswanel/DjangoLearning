ea1f6c2da922406368aebce5f4dadd36ae3f7ff1
geswanel <geswanel@gmail.com>
2024-01-10 07:01:15 +0700

    Profile update forms + Pillow Image resizing

--- tutorial_project/users/models.py
+++ tutorial_project/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 class Profile(models.Model):
     image = models.ImageField(default="default.png", upload_to="profile_pics")
@@ -7,3 +9,14 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} Profile"
+    
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            sizes = (300, 300)
+            img.thumbnail(sizes)
+            img.save(self.image.path)
